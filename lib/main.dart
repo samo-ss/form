26148159b8faeea2e4203da18fd39ef998b99ca4
Saharsh Modi <saharshmodi@Saharshs-MacBook-Pro.local>
2019-06-25 14:35:52 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'contact_service.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
+import 'contact.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Form',
+      theme: new ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: new MyHomePage(title: 'Form'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  Contact newContact = new Contact();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  List<String> _colors = <String>['', 'red', 'green', 'blue', 'orange'];
+  String _color = '';
+
+  final TextEditingController _controller = new TextEditingController();
+  Future _chooseDate(BuildContext context, String initialDateString) async {
+    var now = new DateTime.now();
+    var initialDate = convertToDate(initialDateString) ?? now;
+    initialDate = (initialDate.year >= 1900 && initialDate.isBefore(now) ? initialDate : now);
+
+    var result = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: new DateTime(1900),
+        lastDate: new DateTime.now());
+
+    if (result == null) return;
+
+    setState(() {
+      _controller.text = new DateFormat.yMd().format(result);
+    });
+  }
+
+  DateTime convertToDate(String input) {
+    try
+    {
+      var d = new DateFormat.yMd().parseStrict(input);
+      return d;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+
+    Widget build(BuildContext context) {
+    return new Scaffold(
+      key: _scaffoldKey,
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new SafeArea(
+          top: false,
+          bottom: false,
+          child: new Form(
+          key: _formKey,
+          autovalidate: true,
+          child: new ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            children: <Widget>[
+              new TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.person),
+                  hintText: 'Enter your first and last name',
+                  labelText: 'Name',
+                ),
+                inputFormatters: [new LengthLimitingTextInputFormatter(30)],
+                validator: (val) => val.isEmpty ? 'Name is required' : null,
+                onSaved: (val) => newContact.name = val,
+              ),
+              new Row(children: <Widget>[
+                new Expanded(
+                    child: new TextFormField(
+                      decoration: new InputDecoration(
+                        icon: const Icon(Icons.calendar_today),
+                        hintText: 'Enter your date of birth',
+                        labelText: 'Dob',
+                      ),
+                      controller: _controller,
+                      keyboardType: TextInputType.datetime,
+                      validator: (val) =>
+                      isValidDob(val) ? null : 'Not a valid date',
+                      onSaved: (val) => newContact.dob = convertToDate(val),
+                    )),
+                new IconButton(
+                  icon: new Icon(Icons.more_horiz),
+                  tooltip: 'Choose date',
+                  onPressed: (() {
+                    _chooseDate(context, _controller.text);
+                  }),
+                )
+              ]),
+              new TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.phone),
+                  hintText: 'Enter a phone number',
+                  labelText: 'Phone',
+                ),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [
+                  WhitelistingTextInputFormatter.digitsOnly,
+                ],
+                onSaved: (val) => newContact.phone = val,
+              ),
+              new TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.email),
+                  hintText: 'Enter a email address',
+                  labelText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) => isValidEmail(value)
+                    ? null
+                    : 'Please enter a valid email address',
+                onSaved: (val) => newContact.email = val,
+              ),
+              new FormField<String>(
+                builder: (FormFieldState<String> state) {
+                  return InputDecorator(
+                    decoration: InputDecoration(
+                      icon: const Icon(Icons.color_lens),
+                      labelText: 'Color',
+                      errorText: state.hasError ? state.errorText : null,
+                    ),
+                    isEmpty: _color == '',
+                    child: new DropdownButtonHideUnderline(
+                      child: new DropdownButton<String>(
+                        value: _color,
+                        isDense: true,
+                        onChanged: (String newValue) {
+                          setState(() {
+                            newContact.favoriteColor = newValue;
+                            _color = newValue;
+                            state.didChange(newValue);
+                          });
+                        },
+                        items: _colors.map((String value) {
+                          return new DropdownMenuItem<String>(
+                            value: value,
+                            child: new Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  );
+                },
+                validator: (val) {
+                  return val != '' ? null : 'Please select a color';
+                },
+              ),
+              new Container(
+                  padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+                  child: new RaisedButton(
+                    child: const Text('Submit'),
+                    onPressed: _submitForm,
+                  )),
+            ],
+          ))),
+    );
+  }
+
+  bool isValidDob(String dob) {
+    if (dob.isEmpty) return true;
+    var d = convertToDate(dob);
+    return d != null && d.isBefore(new DateTime.now());
+  }
+
+  bool isValidEmail(String input) {
+    final RegExp regex = new RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+    return regex.hasMatch(input);
+  }
+
+  void _submitForm() {
+    final FormState form = _formKey.currentState;
+
+    if (!form.validate()) {
+      showMessage('Form is not valid!  Please review and correct.');
+    } else {
+      form.save(); //This invokes each onSaved event
+
+      print('Form save called, newContact is now up to date...');
+      print('Email: ${newContact.name}');
+      print('Dob: ${newContact.dob}');
+      print('Phone: ${newContact.phone}');
+      print('Email: ${newContact.email}');
+      print('Favorite Color: ${newContact.favoriteColor}');
+      print('========================================');
+      print('Submitting to back end...');
+      var contactService = new ContactService();
+      contactService.createContact(newContact)
+          .then((value) =>
+          showMessage('New contact created for ${value.name}!', Colors.blue
+          )
+      );    }
+  }
+
+  void showMessage(String message, [MaterialColor color = Colors.red]) {
+    _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(backgroundColor: color, content: new Text(message)));
+  }
+}
+
